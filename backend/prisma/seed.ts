c8b2d3a2e8e6e6398f37f7e3484c9fbd4efab566
LuH3ss <lucashess98@gmail.com>
2026-03-16 17:10:00 -0300

    user admin creeated

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,28 +1,42 @@
-import prisma from '../src/lib/prisma.js'
-import bcrypt from 'bcrypt'
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
 
+const prisma = new PrismaClient();
 
+async function main() {
+  const passwordAdmin = await bcrypt.hash('admin123', 10);
+  const passwordJuli = await bcrypt.hash('juli123', 10);
 
-async function main () {
-  const hashedPassword = await bcrypt.hash('admin123', 10)
+  // Crear o actualizar usuario Juli
+  await prisma.user.upsert({
+    where: { email: 'juli@deleon.com' },
+    update: {},
+    create: {
+      email: 'juli@deleon.com',
+      password: passwordJuli,
+      role: 'ADMIN',
+    },
+  });
 
-  const existingAdmin = await prisma.user.findUnique({
-    where: { email: 'admin@mail.com' }
-  })
-  if (existingAdmin) {
-    console.log('Admin already exists')
-    return
-  }
-  await prisma.user.create({
-    data: { email: 'admin@mail.com', password: hashedPassword, role: 'ADMIN' }
-  })
-  console.log('Admin created successfully')
+  // Crear o actualizar usuario Admin
+  await prisma.user.upsert({
+    where: { email: 'admin@mail.com' },
+    update: {},
+    create: {
+      email: 'admin@mail.com',
+      password: passwordAdmin,
+      role: 'ADMIN',
+    },
+  });
+
+  console.log('Seeders ejecutados correctamente.');
 }
+
 main()
-  .catch(e => {
-    console.error(e)
-    process.exit(1)
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
   })
   .finally(async () => {
-    await prisma.$disconnect()
-  })
+    await prisma.$disconnect();
+  });
